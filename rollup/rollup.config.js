5f1e4f441bc92fed13453ec949a474b33ef32a49
ics-ikeda <ikeda@ics-web.jp>
2017-05-10 15:44:27 +0900

    rollup : 😜babel 👉 😊buble

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,6 +1,5 @@
 import nodeResolve  from 'rollup-plugin-node-resolve';
-import babel        from 'rollup-plugin-babel';
-import commonjs     from 'rollup-plugin-commonjs';
+import buble        from 'rollup-plugin-buble';
 import uglify       from 'rollup-plugin-uglify';
 import {minify}     from 'uglify-js-harmony';
 
@@ -9,10 +8,9 @@ export default {
   dest: 'build/bundle.js',
   plugins: [
     nodeResolve(), // Node.jsのモジュール読み込みに対応
-    commonjs(), // require 系を処理できるようにする
-    babel(), // ES5へトランスパイラする
+    buble(), // ES5へ
     uglify({}, minify) // ファイルを小さくする
   ],
-  format : "iife", // scriptタグの実行に適切な指定
+  format: "iife", // scriptタグの実行に適切な指定
   sourceMap: true // ソースマップの出力
 };

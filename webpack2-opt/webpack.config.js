2851d6102c06ff84c8d34365820895162cf6ff67
ics-ikeda <ikeda@ics-web.jp>
2017-06-23 18:14:13 +0900

    webpack@2 の検証デモを追加

---
+++ webpack2-opt/webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: __dirname + '/src/main.js',
+
+  output: {
+    path    : __dirname + '/build',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test   : /\.js$/,
+        use    : 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.glsl$/,
+        use : 'webpack-glsl-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+  ]
+};
